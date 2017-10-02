66905bcfa1bbbf2fca357465b325a7437705bb0e
Dreg0 <andrii.kruglakov@gmail.com>
2017-10-03 00:52:38 +0300

    wokring on it

--- js/main.js
+++ js/main.js
@@ -1,26 +1,57 @@
 //Listen for form submit
-document.getElementById('myForm').addEventListener('submit', saveBoockmark);
+document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
+//save bookMark
+function saveBookmark(e) {
+  //get form values
+  let siteName = document.getElementById("siteName").value;
+  let siteUrl = document.getElementById("siteUrl").value;
 
-//save boockMark
-function saveBoockmark(e) {
-    //get form values
-    let siteName = document.getElementById('siteName').value;
-    let siteUrl = document.getElementById('siteUrl').value;
+  //pass values of siteName & siteUrl to an object
+  let bookmark = {
+    name: siteName,
+    url: siteUrl
+  };
 
+  //Local Storage Test
+  //localStorage.setItem("test","1234 ");
+  //localStorage.getItem("test")
 
-    //pass values of siteName & siteUrl to an object
-    let boockMark = {
-        name: siteName,
-        url: siteUrl
-    };
+  //test if bookmark is null
+  if (localStorage.getItem("bookmarks") === null) {
+    //initialise array
+    let bookmarks = [];
+    //add to array
+    bookmarks.push(bookmark);
+    //set to local storage JSON object as a string  .stringify
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  } else {
+    //get bookmarks from local storage turning string back to JSON .parse
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    //add new bookmark to array
+    bookmarks.push(bookmark);
+    //set array back to local storage with previous and new added values
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  //prevent from submitting
+  e.preventDefault();
+}
 
-    //Local Storage Test
+//fetching bookmarks from local storage
+function fetchBookmarks() {
+  let bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  //get output id where bookmarks will be shown
+  let bookmarkResults = document.getElementById("bookmarkResults");
+  //build output
+  bookmarkResults.innerHTML = '';
 
-    //prevent from submitting
-    e.preventDefault();
+  for(let i =0; i < bookmarks.length; i++ ){
+    let name = bookmarks[i].name;
+    let url = bookmarks[i].url;
 
-    //just checking
-    console.log("it works");
-    console.log(boockMark);
+    bookmarkResults.innerHTML += '<div class="well">'+
+                                    '<h3>' +name+
+                                    '</h3>'+
+                                    '</div>';
+  }
 }
